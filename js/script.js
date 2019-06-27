920a5237f857a29928ee3401dbb537f450ef2d83
NikitaNevsky <40342871+NikitaNevsky@users.noreply.github.com>
2019-06-27 10:31:50 +0300

    Update and add files on js folder

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
   $(".main-slider").owlCarousel({
         items:1,
-        animateOut: 'slideOutDown',
-    	animateIn: 'flipInX',
+        autoplay:true,
+    	autoplayTimeout:4000,
+    	autoplayHoverPause:true,
         merge:true,
         nav:true,
         navText: ["<img src='images/prev.svg' class='left'>","<img src='images/next.svg' class='right'>"],
@@ -16,6 +17,9 @@ $(document).ready(function(){
   $(".mini-slider").owlCarousel({
 	  	loop:true,
 	    nav:true,
+	    autoplay:true,
+    	autoplayTimeout:4000,
+    	autoplayHoverPause:true,
 	    responsive:{
 	        0:{
 	            items:1
@@ -31,4 +35,24 @@ $(document).ready(function(){
         navClass: ['owl-prev','owl-next'],
         dots:false,
     });                
+});
+
+$(document).ready(function() {
+	$('.gallery-item').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		}
+	});
 });
